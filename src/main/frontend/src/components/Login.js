883fc4e296a5220816590ee9e6f46bf7f944e0df
GunwooJo <kanggi1997@naver.com>
2023-09-17 16:57:57 +0900

    react-router-dom 셋팅완료

---
+++ src/main/frontend/src/components/Login.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Login() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Example: send this to an API for authentication
+    console.log('Username:', username, 'Password:', password);
+
+    // TODO: Handle actual authentication logic here
+  };
+
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/main/frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import Login from "../components/Login";
+import '../styles/login.css';
+
+function Home() {
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default Home;
