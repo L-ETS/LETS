5ef0259888a81ce4161c0a2d411517d3f0066ee0
GunwooJo <kanggi1997@naver.com>
2023-09-21 16:09:33 +0900

    react와 nodejs를 이용한 초기셋팅 완료

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//유저가 보낸 object, array데이터 출력해보기 위함.
+app.use(express.json());
+
+//다른 도메인 주소끼리 ajax요청 가능하게함.
+let cors = require('cors');
+app.use(cors());
+
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+app.listen(8080, function () {
+  console.log('listening on 8080')
+}); 
+
+//이 코드는 반드시 가장 하단에 놓여야 함. 고객에 URL란에 아무거나 입력하면 index.html(리액트 프로젝트 빌드파일)을 전해달란 의미.
+app.get('*', function (request, response) {
+  response.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
