d0a42fa1ee880d9e14b2991695d6dca9768b7bf2
GunwooJo <kanggi1997@naver.com>
2023-09-22 21:28:30 +0900

    로그인 폼 내용 서버로 전송하는 함수 대략적인 틀 작성

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,15 +4,28 @@ import '../styles/login.css';
 
 function Login() {
   
-  const [userId, setUserId] = useState('');
-  const [password, setPassword] = useState('');
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
 
   const navigate = useNavigate();
 
   //로그인 버튼 눌렀을 때 벌어질 이벤트
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
+    try {
+      await axios.post('/user/login', {
+        id: id,
+        pw: pw
+      });
+      // 로그인 성공했을 때의 코드 작성.
+      // For example, you might store the JWT token in local storage and redirect the user to the home page
+      alert('로그인 성공');
+      navigate('/')
+
+    } catch(error) {
+      console.log('login error: ', error);
+    }
    
   };
 
@@ -21,24 +34,24 @@ function Login() {
       <h2>로그인</h2>
       <form onSubmit={handleSubmit}>
         <div className="input-group">
-          <label htmlFor="userId">아이디</label>
+          <label htmlFor="id">아이디</label>
           <input
             type="text"
-            id="userId"
-            value={userId}
+            id="id"
+            value={id}
             maxLength={30}
-            onChange={(e) => setUserId(e.target.value)}
+            onChange={(e) => setId(e.target.value)}
             required
           />
         </div>
         <div className="input-group">
-        <label htmlFor="password">비밀번호</label>
+        <label htmlFor="pw">비밀번호</label>
           <input
             type="password"
-            id="password"
-            value={password}
+            id="pw"
+            value={pw}
             maxLength={30}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPw(e.target.value)}
             required
           />
         </div>
