d8cabc7c87b0916e93379f0ebac28519d2e03e4a
SDY <144594556+CBG24@users.noreply.github.com>
2023-10-12 21:35:07 +0900

    게시글 개별 조회/업로드

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const mysql = require("mysql2");
 const session = require('express-session');
 const MySQLStore = require("express-mysql-session")(session);
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
 
 //유저가 보낸 object, array데이터 출력해보기 위함.
 app.use(express.json());
@@ -37,6 +40,23 @@ const pool = mysql.createPool({
   connectionLimit: 10
 });
 
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESSKEY,
+  secretAccessKey: process.env.S3_SECRETKEY,
+  region: process.env.S3_REGION,
+});
+
+let upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      cb(null, `productImages/${Date.now()}_${file.originalname}`);
+    },
+  }),
+});
 
 app.use(cookieParser());
 
@@ -165,6 +185,112 @@ app.get('/api/check-session', (req, res) => {
 
 app.get('/user/mypage', isAuthenticated, (req, res) => {
   res.status(200).json({success: true})
+})
+
+app.post('', isAuthenticated, upload.array('images'), (req, res) => { //게시글 업로드
+
+  const uploadedFiles = req.files.map(file => ({
+      originalName: file.originalname,
+      s3ObjectName: file.key,
+      s3Url: file.location
+  }));
+  uploadedFiles.map((file=>{
+    console.log(`file: ${file}`);
+    console.log(`file.originalname: ${JSON.stringify(file.originalName)}`);
+    
+  }))  
+
+  const { title, content } = {...req.body};
+  
+  pool.getConnection((error, connection)=>{
+    if(error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Database connection error.' });
+    }
+    else {
+      //post테이블에 게시글 정보 저장.
+      let sql = 'INSERT INTO post (userId, title, content) VALUES (?, ?, ?)';
+      let params = [req.session.user, title, content];
+      connection.query(sql, params, async (error, result)=>{
+        if(error) {
+          console.error('Error executing the query: '+ error.stack)
+          connection.release();
+          return res.status(500).json({message: 'db문제 발생.'});
+        }
+        else {
+          //image테이블에 이미지 정보 저장.
+          const promises = req.files.map(file => {
+            return new Promise((resolve, reject) => {
+              sql = 'INSERT INTO image (postId, imageName, imageUrl) VALUES (?, ?, ?)';
+              params = [result.insertId, file.originalname, file.location];
+              connection.query(sql, params, (error) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve();
+                }
+              });
+            });
+          });
+
+          try {
+            await Promise.all(promises);
+            connection.release();
+            res.status(200).json({ message: '저장완료' });
+          } catch (error) {
+            console.error('Error executing the query: ' + error.stack);
+            connection.release();
+            return res.status(500).json({ message: 'db문제 발생.' });
+          }
+
+        }
+      })
+    }
+  })
+});
+
+app.post('',isAuthenticated, (req, res) => { //특정 게시글 출력
+  const postId = req.body.postId;
+
+  let sql = 'SELECT * FROM POST WHERE postId = ?';
+  let params = [postId];
+
+  pool.getConnection((error, connection)=>{
+    if(error) {
+      console.log(error);
+    }
+    else {
+      connection.query(sql, params, (error, result)=>{
+        if(error) {
+          console.error('Error executing the query: '+ error.stack)
+          res.status(401).json({message: 'db조회 실패'});
+          connection.release();
+        }
+        else {
+          let post = result[0]
+          let images;
+          if (!post) {
+            connection.release();
+            return res.status(401).json({ error: 'Invalid postId' });
+          }
+          sql = 'SELECT * FROM image WHERE postId = ?';
+          params = [postId];
+          connection.query(sql, params, (error, result) => {
+            if (error) {
+              console.error('Error executing the query: '+ error.stack)
+              res.status(401).json({message: 'db조회 실패'});
+              connection.release();
+            } else {
+              images = result[0];
+            }
+          });
+
+          res.render('post', {post:result[0], images:images});
+          connection.release();
+        }
+      })
+    }
+  })
 })
 
 //이 코드는 반드시 가장 하단에 놓여야 함. 고객에 URL란에 아무거나 입력하면 index.html(리액트 프로젝트 빌드파일)을 전해달란 의미.
