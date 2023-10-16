8cac144604ffb7bbe027aab4f7c7abdd0330dfb7
GunwooJo <kanggi1997@naver.com>
2023-10-17 03:06:20 +0900

    design: 상세페이지 배치 변경(왼쪽 사진, 오른쪽 글)

--- client/src/pages/PostDetail.js
+++ client/src/pages/PostDetail.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
+import '../styles/postdetail.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function PostDetail() {
   const { postId } = useParams();
@@ -31,53 +35,58 @@ function PostDetail() {
   
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      {
-        isMyPost ? 
-        <div>
-          <Button variant="success">수정</Button>
-          <Button variant="danger">삭제</Button>
-        </div>
-        : 
-        null
-      }
-      <div style={{width: '410px', height: '410px'}}>
-        <img style={{height: '410px'}} src={mainImageSrc} alt="Main Preview" fluid />
-      </div>
-        {
-          images.map((image, index)=>{
-            return (
+    <Container className="container">
+      <Row>
+        <Col>
+          <h2>{post.title}</h2>
+          {
+            isMyPost ? 
+            <div>
+              <Button variant="success">수정</Button>
+              <Button variant="danger">삭제</Button>
+            </div>
+            : 
+            null
+          }
+          <div >
+            <img style={{maxWidth: '100%', height: '410px'}} src={mainImageSrc} alt="Main Preview" fluid />
+          </div>
+            {
+              images.map((image, index)=>{
+                return (
 
-              <div key={index} style={{display:"inline-block"}}>
-                <img  width='40px' src={image.imageUrl} onMouseEnter={() => setMainImageSrc(image.imageUrl)}/>
+                  <div key={index} style={{display:"inline-block"}}>
+                    <img  width='40px' src={image.imageUrl} onMouseEnter={() => setMainImageSrc(image.imageUrl)}/>
+                  </div>
+                  
+                )
+              })
+            }
+        </Col>
+        <Col>
+          <div style={{margin: '10px 0', borderBottom: '1px solid'}}>
+            {
+              create < update ?
+              <div>
+                <span>작성자: {post.userId}</span>
+                <br/>
+                <span>수정일: {update.getFullYear()}년 {update.getMonth()+1}월 {update.getDate()}일</span>            
               </div>
-              
-            )
-          })
-        }
-      <div style={{margin: '10px 0', borderBottom: '1px solid'}}>
-        {
-          create < update ?
-          <div>
-            <span>작성자: {post.userId}</span>
-            <br/>
-            <span>수정일: {update.getFullYear()}년 {update.getMonth()+1}월 {update.getDate()}일</span>            
+              :
+              <div>
+                <span>작성자: {post.userId}</span>
+                <br/>
+                <span>작성일: {create.getFullYear()}년 {create.getMonth()+1}월 {create.getDate()}일</span>
+              </div>
+            }
           </div>
-          :
-          <div>
-            <span>작성자: {post.userId}</span>
-            <br/>
-            <span>작성일: {create.getFullYear()}년 {create.getMonth()+1}월 {create.getDate()}일</span>
-          </div>
-        }
-      </div>
-      
-      <p>
-        {post.content}
-      </p>
-    
-    </div>
+          
+          <p>
+            {post.content}
+          </p>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
