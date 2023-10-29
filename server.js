5aef65a87987ed24c3f92fdd0ef2d2c3eafaf3e8
chobumjun <fuckjizz@naver.com>
2023-10-29 18:05:42 +0900

    postCount

--- server.js
+++ server.js
@@ -349,32 +349,36 @@ app.get('/posts/:postId',isAuthenticated, (req, res) => { //특정 게시글 출
                   // The view_count was increased
                   res.status(200).json({ message: 'View count updated successfully.', post: post, images: images, isMyPost: isMyPost});
                 }
-              })
-
-              const post_id = [1, 2, 3]; // 특정 postid 배열 (임의)
-
-              // 배열 루프 and 각 id마다 쿼리 실행
-              post_id.forEach((postId) => {
-                // 특정 postId count + 1 하는 쿼리
-                const sqlQuery = `
-                  INSERT INTO UserPostCounts (post_id, count)
-                  VALUES (?, 1)
-                  ON DUPLICATE KEY UPDATE count = count + 1
-                `;
-              
-                const values = [postId];
               });
-              
-              connection.release();
-            }
+            }          
           });
-
-          
         }
-      })
+      });
     }
-  })
-})
+    const post_id = [1, 2, 3]; // 특정 postid 배열 (임의)
+
+      // 배열 루프 and 각 id마다 쿼리 실행
+      post_id.forEach((postId) => {
+      // 특정 postId count + 1 하는 쿼리
+        const sqlQuery = `
+          INSERT INTO UserPostCounts (post_id, count)
+          VALUES (?, 1)
+          ON DUPLICATE KEY UPDATE count = count + 1
+        `;
+         
+        const values = [postId];
+
+        connection.query(sqlQuery, values, (error, results, fields) => {
+          if (error) {
+            console.error('Error executing query:', error);
+          } else {
+            console.log('Query executed successfully');
+          }
+        });
+      });
+      connection.release();
+  });
+});
 
 //이 코드는 반드시 가장 하단에 놓여야 함. 고객에 URL란에 아무거나 입력하면 index.html(리액트 프로젝트 빌드파일)을 전해달란 의미.
 app.get('*', function (request, response) {
