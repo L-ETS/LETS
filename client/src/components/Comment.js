d94b61dee287ce4f7f8227a147055844d999d026
GunwooJo <kanggi1997@naver.com>
2023-11-11 21:26:31 +0900

    feat: 채팅버튼 관련 onclick함수 작성.

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -45,6 +45,35 @@ function Comment({comment}) {
     }
   }
 
+  //채팅방으로 이동.
+  const handleChat = async () => {
+    let user1, user2;
+
+    if(logginedUserId < comment.userId) {
+      user1 = logginedUserId;
+      user2 = comment.userId;
+    } else {
+      user1 = comment.userId;
+      user2 = logginedUserId;
+    }
+
+    if(!user1 || !user2) throw new Error('user1 또는 user2 정보 없음.');
+
+    try {
+      const response = await axios.get(`/chat/enterChat/${user1}/${user2}`);
+      const uuid = response.data.uuid;
+
+      if(!uuid) {
+        throw new Error('uuid 값이 없음.');
+      }
+      navigate(`/chat/${uuid}`);
+    } catch (error) {
+      alert('문제가 발생했습니다. 다시 시도해주세요.');
+      console.log(error);
+    }
+    
+  }
+
   return (
     <div className='reply'>
       <div>
@@ -55,7 +84,8 @@ function Comment({comment}) {
             <button className='edit'onClick={handleEdit} style={{borderRadius: '5px'}}>수정</button>
             <button className='delete'onClick={handleDelete} style={{borderRadius: '5px'}}>삭제</button>
           </div>
-          : null
+          : 
+          <button onClick={handleChat}>채팅</button>
         }        
       </div>          
       
