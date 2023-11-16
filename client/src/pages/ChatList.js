1ed2ae282ba44074e064b738c82a2bb404e97dc6
ourumo <ss8638@naver.com>
2023-11-16 17:22:16 +0900

    fix: ChatList에서 logginedUserId 사용 완료

--- client/src/pages/ChatList.js
+++ client/src/pages/ChatList.js
@@ -4,28 +4,32 @@ import UserContext from "../contexts/UserContext";
 
 function ChatList() {
   const { logginedUserId } = useContext(UserContext); //현재 로그인한 유저의 id
-  const user = logginedUserId;
   const [roomuuid, setRoomuuid] = useState([]);
 
   useEffect(() => {
-    if (user == null) return; // 이부분이 없으면 null값이 한 번 출력됨
-    axios.get(`/${user}/getChatlist`)
-      .then(res => {
-        //console.log("chatlist: ", res.data);
-        //console.log(res.data.chatList);
-        let myChatList = res.data.chatList;
-        myChatList.map((myRoomuuid, index) => {
-            roomuuid.push(myRoomuuid.uuid);
-        });
-      })
-      .catch((error) => {
-        console.error('데이터 가져오기 오류:', error);
-      })
-  }, [user]);
+    if (logginedUserId !== null) {
+      axios.get(`/${logginedUserId}/getChatlist`)
+        .then(res => {
+            console.log("유저는 ", logginedUserId);
+            //console.log("chatlist: ", res.data);
+            //console.log(res.data.chatList);
+            let myChatList = res.data.chatList;
+            myChatList.map((myRoomuuid, index) => {
+                roomuuid.push(myRoomuuid.uuid);
+            });
+        })
+        .catch((error) => {
+            console.error('데이터 가져오기 오류:', error);
+        })
+    }
+  },[logginedUserId]);
 
   return (
     <div>
       <h1>챗 리스트</h1>
+      {
+        roomuuid.map((value, key) => <div>{value}</div>) // 이미지 1번(포스트), 마지막 채팅 내용이랑 시간(파이어베이스), 유저 이름(챗룸)
+      }
     </div>
   )
 }
