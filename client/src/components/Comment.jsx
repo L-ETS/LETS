6c6341c804556b4d26bd39de10e60bcf16ce6240
GunwooJo <kanggi1997@naver.com>
2023-11-16 19:06:18 +0900

    feat: 댓글 삭제버튼 누르면 경고 띄우기

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -3,9 +3,12 @@ import axios from "axios";
 import '../styles/reply.css';
 import UserContext from "../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
+import Modal from 'react-bootstrap/Modal';  
+import Button from 'react-bootstrap/Button';
 
 function Comment({comment, comments, setComments}) {
   const [commentContent, setCommentContent] = useState('');  
+  const [showAlert, setShowAlert] = useState(false);
   const { logginedUserId } = useContext(UserContext); //현재 로그인한 유저의 id
   const navigate = useNavigate();
 
@@ -47,13 +50,27 @@ function Comment({comment, comments, setComments}) {
 
   return (
     <div className='reply'>
+      <Modal show={showAlert} onHide={()=>{setShowAlert(false)}}>
+        <Modal.Header closeButton>
+          <Modal.Title>정말로 삭제하시겠습니까?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>삭제 후 복구할 수 없습니다.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={()=>{setShowAlert(false)}}>
+            취소
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            삭제
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div>
         <div className='writer'>{comment.userId}</div>
         {
           comment.userId === logginedUserId ? //comment.userId는 댓글 작성자의 id
           <div>
             <button className='edit'onClick={handleEdit} style={{borderRadius: '5px'}}>수정</button>
-            <button className='delete'onClick={handleDelete} style={{borderRadius: '5px'}}>삭제</button>
+            <button className='delete'onClick={()=>setShowAlert(true)} style={{borderRadius: '5px'}}>삭제</button>
           </div>
           : null
         }        
