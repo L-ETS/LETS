5b9180147098abfb3f25b5a114187847eee53627
SDY <144594556+CBG24@users.noreply.github.com>
2023-11-17 23:33:25 +0900

    채팅방 ui

---
+++ client/src/pages/ChatList.js
@@ -0,0 +1,76 @@
+import React from "react";
+
+function ChatList() {
+  return (
+    <div className="ChatList">
+      <div class="row rounded-lg overflow-hidden">
+        <div class="bg-white">
+          <div class="bg-gray px-4 py-2 bg-light">
+            <p class="h5 mb-0 py-1">채팅</p>
+          </div>
+          <div class="chat-box">
+            <div class="list-group rounded-0">
+              <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+                <div class="media"><img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b4d8c220-f514-4a01-af92-6ef5b413975c?apiKey=d405d86acb3a46ca93e43749ab4789b2&width=100"
+                  alt="Item" width="100" class="rounded-circle" />
+                  <div class="media-body ml-4">
+                    <div class="d-flex align-items-center justify-content-between mb-1">
+                      <h5 class="mb-0">조건우</h5><small class="small font-weight-bold">1초 전</small>
+                    </div>
+                    <p class="font-italic text-muted mb-0 text-small">m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요/m3 맥북 주세요</p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+                <div class="media"><img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b4d8c220-f514-4a01-af92-6ef5b413975c?apiKey=d405d86acb3a46ca93e43749ab4789b2&width=100"
+                  alt="Item" width="100" class="rounded-circle" />
+                  <div class="media-body ml-4">
+                    <div class="d-flex align-items-center justify-content-between mb-1">
+                      <h5 class="mb-0">손정민</h5><small class="small font-weight-bold">5시간 전</small>
+                    </div>
+                    <p class="font-italic text-muted mb-0 text-small">저 없었으면 어떡할 뻔 했나요</p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+                <div class="media"><img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b4d8c220-f514-4a01-af92-6ef5b413975c?apiKey=d405d86acb3a46ca93e43749ab4789b2&width=100"
+                  alt="Item" width="100" class="rounded-circle" />
+                  <div class="media-body ml-4">
+                    <div class="d-flex align-items-center justify-content-between mb-1">
+                      <h5 class="mb-0">이규태</h5><small class="small font-weight-bold">하루 전</small>
+                    </div>
+                    <p class="font-italic text-muted mb-0 text-small">저만 믿고 따라오세요</p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+                <div class="media"><img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b4d8c220-f514-4a01-af92-6ef5b413975c?apiKey=d405d86acb3a46ca93e43749ab4789b2&width=100"
+                  alt="Item" width="100" class="rounded-circle" />
+                  <div class="media-body ml-4">
+                    <div class="d-flex align-items-center justify-content-between mb-1">
+                      <h5 class="mb-0">오현승</h5><small class="small font-weight-bold">일주일 전</small>
+                    </div>
+                    <p class="font-italic text-muted mb-0 text-small">디자인 마스터 했습니다</p>
+                  </div>
+                </div>
+              </a>
+              <a href="#" class="list-group-item list-group-item-action list-group-item-light rounded-0">
+                <div class="media"><img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b4d8c220-f514-4a01-af92-6ef5b413975c?apiKey=d405d86acb3a46ca93e43749ab4789b2&width=100"
+                  alt="Item" width="100" class="rounded-circle" />
+                  <div class="media-body ml-4">
+                    <div class="d-flex align-items-center justify-content-between mb-1">
+                      <h5 class="mb-0">조범준</h5><small class="small font-weight-bold">4달 전</small>
+                    </div>
+                    <p class="font-italic text-muted mb-0 text-small">여러분 사랑합니다</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChatList;
