822094b7aeefbb42772ea58a9fe0f3e7598181f1
GunwooJo <kanggi1997@naver.com>
2023-11-20 11:46:54 +0900

    feat: 유저 정보 가져오는 함수 작성

--- client/src/pages/MyInform.js
+++ client/src/pages/MyInform.js
@@ -1,12 +1,38 @@
-import React from "react";
+import {React, useEffect, useContext, useState} from "react";
 import Button from 'react-bootstrap/Button';
 import styles from '../styles/MyInform.module.css';
 import { useNavigate } from 'react-router-dom';
-
+import UserContext from "../contexts/UserContext";
+import axios from "axios";
 
 function MyInform() {
 
   const routing =  useNavigate();
+  const { logginedUserId } = useContext(UserContext); //현재 로그인된 유저의 아이디
+  const [userId, setUserId] = useState('');
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [wideRegion, setWideRegion] = useState('');
+  const [detailRegion, setDetailRegion] = useState('');
+
+  const fetchUserInfo = async () => {
+    try {
+      const response = await axios.get(`/user/fetchInfo`);
+      setUserId(response.data.user.userId);
+      setEmail(response.data.user.email);
+      setNickname(response.data.user.nickname);
+      setWideRegion(response.data.user.wideRegion);
+      setDetailRegion(response.data.user.detailRegion);
+    } catch (error) {
+      console.log(error);
+      alert('오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
+    }
+  }
+  /* 백엔드 코드와 연동 후 주석해제.
+  useEffect(()=>{
+    fetchUserInfo();
+  },[])
+  */
 
   function goEditMyPage() {
 		routing('/posts/editmyinform');
